4e86b06fb0b060e0c5ea3fa111f5ee089b72ec18
Tim Smith <tim@cozy.co>
2015-03-29 17:19:44 -0700

    Bump version for development

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Sets up iptables to use a script to maintain rules'
-version '1.0.0'
+version '1.0.1'
 
 recipe 'iptables', 'Installs iptables and sets up .d style config directory of iptables rules'
 %w(redhat centos debian ubuntu amazon scientific oracle amazon).each do |os|
