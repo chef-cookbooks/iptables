9c7c25e62217d0c2d4b530cf4a0cb37be4d7d723
Chris Zeeb <chris.zeeb@gmail.com>
2015-09-04 17:07:44 -0400

    Bringing unit test coverage up to 100%

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
+RSpec.configure do |config|
+  config.log_level = :error
+  config.platform = 'ubuntu'
+  config.version = '14.04'
+end
+
 at_exit { ChefSpec::Coverage.report! }
