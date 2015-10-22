123f2d51c2d0b1febde44f53a60eadfbff1d4a1f
Tim Smith <tsmith84@gmail.com>
2015-10-21 23:48:52 -0700

    Service should only be disabled on RHEL

--- spec/unit/recipes/disabled_spec.rb
+++ spec/unit/recipes/disabled_spec.rb
@@ -7,12 +7,18 @@ describe 'iptables::disabled' do
     expect(chef_run).to install_package('iptables')
   end
 
-  it 'disables and stops iptables service' do
-    expect(chef_run).to disable_service('iptables')
-    expect(chef_run).to stop_service('iptables')
-  end
-
   it 'deletes /etc/iptables.d directory' do
     expect(chef_run).to delete_directory('/etc/iptables.d')
+  end
+
+  context 'rhel 7' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'redhat', version: '7.0').converge(described_recipe)
+    end
+
+    it 'disables and stops iptables service' do
+      expect(chef_run).to disable_service('iptables')
+      expect(chef_run).to stop_service('iptables')
+    end
   end
 end
