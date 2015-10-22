0596400d9900238390e58d738150d39a8521838a
Tim Smith <tsmith84@gmail.com>
2015-10-21 23:19:12 -0700

    Add disabled suite and move tests over to enabled
    Add more serverspec tests and test the disabled scenario

---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,34 @@
+require 'serverspec'
+
+set :backend, :exec
+
+# rules no longer ship by default
+describe iptables do
+  it { should_not have_rule('-A FWR -p tcp -m tcp --dport 22 -j ACCEPT') }
+  it { should_not have_rule('-A INPUT -j FWR') }
+  it { should_not have_rule('-A FWR -i lo -j ACCEPT') }
+  it { should_not have_rule('-A FWR -p tcp -m tcp --tcp-flags SYN,RST,ACK SYN -j REJECT --reject-with icmp-port-unreachable') }
+  it { should_not have_rule('-A FWR -p udp -j REJECT --reject-with icmp-port-unreachable') }
+end
+
+if os[:family] == 'redhat'
+  describe service('iptables') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
+
+# the disable recipe will delete this, but the install should add it back
+describe file('/etc/iptables.d') do
+  it { should be_directory }
+end
+
+describe file('/usr/sbin/rebuild-iptables') do
+  it { should exist }
+end
+
+if ['debian', 'ubuntu'].include?(os[:family])
+  describe file('/etc/network/if-pre-up.d/iptables_load') do
+    it { should exist }
+  end
+end
---
+++ test/integration/disabled/serverspec/disabled_spec.rb
@@ -0,0 +1,15 @@
+require 'serverspec'
+
+set :backend, :exec
+
+if os[:family] == 'redhat'
+  describe service('iptables') do
+    it { should_not be_enabled }
+    it { should_not be_running }
+  end
+end
+
+# the disable recipe will delete this, but the install should add it back
+describe file('/etc/iptables.d') do
+  it { should_not be_directory }
+end
