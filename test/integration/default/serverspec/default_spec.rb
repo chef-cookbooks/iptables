422d52223b9d2547513d380256d243150c6075cd
Tim Smith <tsmith84@gmail.com>
2016-02-17 10:15:33 -0800

    Fix default specs to work properly on RHEL
    Also remove the old checks for the rules we removed.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,19 +2,9 @@ require 'serverspec'
 
 set :backend, :exec
 
-# rules no longer ship by default
-describe iptables do
-  it { should_not have_rule('-A FWR -p tcp -m tcp --dport 22 -j ACCEPT') }
-  it { should_not have_rule('-A INPUT -j FWR') }
-  it { should_not have_rule('-A FWR -i lo -j ACCEPT') }
-  it { should_not have_rule('-A FWR -p tcp -m tcp --tcp-flags SYN,RST,ACK SYN -j REJECT --reject-with icmp-port-unreachable') }
-  it { should_not have_rule('-A FWR -p udp -j REJECT --reject-with icmp-port-unreachable') }
-end
-
 if os[:family] == 'redhat'
-  describe service('iptables') do
-    it { should be_enabled }
-    it { should be_running }
+  describe command('/etc/init.d/iptables status') do
+    its(:stdout) { should match /Table: filter/ }
   end
 end
 
@@ -33,7 +23,7 @@ if ['debian', 'ubuntu'].include?(os[:family])
   end
 end
 
-if ['centos', 'fedora'].include?(os[:family])
+if ['redhat', 'fedora'].include?(os[:family])
   describe file('/etc/sysconfig/iptables-config') do
     its(:content) { should match /IPTABLES_STATUS_VERBOSE="yes"/ }
   end
