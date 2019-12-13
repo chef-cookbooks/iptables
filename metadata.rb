bdc44a7ce8f7de1da8e6847b01351986484ecebb
Tim Smith <tsmith84@gmail.com>
2019-12-12 16:31:22 -0800

    Remove recipe and long_description metadata
    Neither of these are used anymore
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,11 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs the iptables daemon and provides a LWRP for managing rules'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '4.5.0'
-
-recipe 'default', 'Installs iptables and sets up .d style config directory of iptables rules'
-recipe 'disabled', 'Disables iptables'
 
 %w(redhat centos debian ubuntu amazon scientific oracle amazon zlinux).each do |os|
   supports os
