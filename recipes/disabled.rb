67d069c2487e2268d7b18c9128890f72a321815b
Tim Smith <tsmith84@gmail.com>
2019-12-16 10:53:44 -0800

    Simplify platform check logic
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/disabled.rb
+++ recipes/disabled.rb
@@ -24,7 +24,7 @@ include_recipe 'iptables::_package'
     action [:disable, :stop]
     delayed_action :stop
     supports status: true, start: true, stop: true, restart: true
-    only_if { %w(rhel fedora amazon).include?(node['platform_family']) }
+    only_if { platform_family?('rhel', 'fedora', 'amazon') }
   end
 
   # Necessary so that if iptables::disable is used and then later
@@ -38,7 +38,7 @@ include_recipe 'iptables::_package'
   ["/etc/sysconfig/#{ipt}", "/etc/sysconfig/#{ipt}.fallback"].each do |f|
     file f do
       content '# iptables rules files cleared by chef via iptables::disabled'
-      only_if { %w(rhel fedora amazon).include?(node['platform_family']) }
+      only_if { platform_family?('rhel', 'fedora', 'amazon') }
       notifies :run, "execute[#{ipt}Flush]", :immediately
     end
   end
